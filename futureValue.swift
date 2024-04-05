a09589013e2c9b49b6fb1792693ce655455452f3
Msesho <155904735+Msesho@users.noreply.github.com>
2024-04-05 11:56:45 +0200

    Create futureValue.swift
    The future value annuity function coded in swift

---
+++ futureValue.swift
@@ -0,0 +1,29 @@
+/*The power function */
+func pow(base: Double, radix: Int): Double {
+  var i = 0
+  while i < radix {
+    base = base * base
+    i++
+  }
+  return base
+}
+
+/*The future value function */
+func futureValue(x: Double, i: Double, n: Int, comp: String): Double {
+  let fv = (x * pow(1 + i, n) - x) / i
+  switch comp {
+    case "monthly":
+      n = n * 12
+      i = (i / 100) / 12
+    case "quarterly":
+      n = n * 4
+      i = (i / 100) / 4
+    case "half-yearly", "biennial":
+      n = n * 2
+      i = (i / 100) / 2
+    default:
+      n = n * 1
+      i = i / 100
+  }
+  return fv
+}
